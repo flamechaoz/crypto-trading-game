f39c621f7ce7786fd3544cefe72f5f749c2e54d5
Jerald Jon Pablo <jeraldjonpablo@gmail.com>
2023-01-04 06:21:37 +0800

    added binance web socket for price ticker

--- src/pages/trade.page.tsx
+++ src/pages/trade.page.tsx
@@ -1,16 +1,93 @@
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Container, Grid, Typography } from '@mui/material';
+import { useState } from 'react';
 import { AdvancedChart } from 'react-tradingview-embed';
 
-const TradePage = (): JSX.Element => {
+const TopBar = (): JSX.Element => {
+  return (
+    <Grid container>
+      <Grid item xs={2}>
+        <TokenMenu />
+      </Grid>
+      <Grid item xs={10}>
+        <LivePriceTicker />
+      </Grid>
+    </Grid>
+  );
+};
+
+const TokenMenu = (): JSX.Element => {
+  return <Typography>Menu</Typography>;
+};
+
+const LiveChart = (): JSX.Element => {
   const tvWidgetProps = {
     interval: '1D',
     theme: 'dark',
     style: '1',
     save_image: false,
-    symbol: 'BTCUSD',
-    studies: ['TripleEMA@tv-basicstudies'],
+    symbol: 'BTCUSDT',
+    studies: [],
   };
 
+  return <AdvancedChart widgetProps={tvWidgetProps} />;
+};
+
+const LivePriceTicker = (): JSX.Element => {
+  const [price, setPrice] = useState(0);
+  const ws = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@trade');
+
+  ws.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+    try {
+      setPrice(Number.parseFloat(data.p).toFixed(2));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return <Typography>{price}</Typography>;
+};
+
+const Orders = (): JSX.Element => {
+  return (
+    <>
+      <Box
+        sx={{
+          width: '100%',
+          height: '100%',
+          backgroundColor: 'primary.dark',
+          '&:hover': {
+            backgroundColor: 'primary.main',
+            opacity: [0.9, 0.8, 0.7],
+          },
+        }}
+      >
+        Order Book
+      </Box>
+    </>
+  );
+};
+
+const BuySellForm = (): JSX.Element => {
+  return (
+    <>
+      <Box
+        sx={{
+          width: '100%',
+          height: '100%',
+          backgroundColor: 'primary.dark',
+          '&:hover': {
+            backgroundColor: 'primary.main',
+          },
+        }}
+      >
+        Buy/Sell
+      </Box>
+    </>
+  );
+};
+
+const TradePage = (): JSX.Element => {
   return (
     <Container
       maxWidth={false}
@@ -18,25 +95,27 @@ const TradePage = (): JSX.Element => {
         backgroundColor: '#2363eb',
         minHeight: '100%',
         position: 'fixed',
+        padding: '0px 0px !important',
       }}
     >
-      <Box
-        maxWidth="lg"
-        sx={{
-          backgroundColor: '#ece9e9',
-          maxHeight: '20rem',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          p: '2rem',
-          mx: 'auto',
-        }}
-      >
-        <Typography variant="h2" component="h1" sx={{ color: '#1f1e1e', fontWeight: 500 }}>
-          Trading Page
-        </Typography>
-      </Box>
-      <AdvancedChart widgetProps={tvWidgetProps} />
+      <Grid container>
+        <Grid item xs={8}>
+          <Grid item direction="column">
+            <Grid item>
+              <TopBar />
+            </Grid>
+            <Grid item>
+              <LiveChart />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={2}>
+          <Orders />
+        </Grid>
+        <Grid item xs={2}>
+          <BuySellForm />
+        </Grid>
+      </Grid>
     </Container>
   );
 };
