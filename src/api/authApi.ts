687c7013e7017117f310951e18ac44f9cb5b6d20
Jerald Jon Pablo <jeraldjonpablo@gmail.com>
2023-01-13 20:56:04 +0800

    implement env vars

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -3,7 +3,7 @@ import Cookies from 'universal-cookie';
 import { LoginInput } from '../pages/login.page';
 import { RegisterInput } from '../pages/register.page';
 import { ITokens, IAuthResponse, IUser } from './types';
-const BASE_URL = 'http://localhost:3001/v1/';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const authApi = axios.create({
   baseURL: BASE_URL,
