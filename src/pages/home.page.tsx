d28c5360f31e116eefa7d48e62af128185c6c576
Jerald Jon Pablo <jeraldjonpablo@gmail.com>
2023-01-19 21:27:57 +0800

    add market overview in home page

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -1,20 +1,33 @@
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Container, Grid, Typography } from '@mui/material';
+import { CryptocurrencyMarket } from 'react-tradingview-embed';
+const BASE_URL = import.meta.env.VITE_WEB_BASE_URL;
 
 const HomePage = (): JSX.Element => {
+  const cmWidgetProps = {
+    colorTheme: 'dark',
+    largeChartUrl: `${BASE_URL}/trade/`,
+    width: '100%',
+  };
+
   return (
-    <Container maxWidth={false} sx={{ minHeight: '100%', pt: '5rem', position: 'fixed' }}>
+    <Container maxWidth={false} sx={{ minHeight: '100%', pt: '3rem' }}>
       <Box
         sx={{
-          height: '15rem',
           display: 'flex',
           alignItems: 'center',
           justifyContent: 'center',
-          mx: 'auto',
         }}
       >
-        <Typography variant="h2" component="h1" sx={{ fontWeight: 500 }}>
-          Home Page
-        </Typography>
+        <Grid direction="column" container className="text-center" sx={{ px: '5rem' }}>
+          <Grid item>
+            <Typography variant="h4" component="h1" sx={{ fontWeight: 500 }}>
+              Your new partner in Crypto Trading
+            </Typography>
+          </Grid>
+          <Grid item sx={{ p: '3rem' }}>
+            <CryptocurrencyMarket widgetProps={cmWidgetProps} />
+          </Grid>
+        </Grid>
       </Box>
     </Container>
   );
