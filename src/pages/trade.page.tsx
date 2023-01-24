68f8a624e6fff845729a8b9ab47eb848f945f47a
Jerald Jon Pablo <jeraldjonpablo@gmail.com>
2023-01-25 04:32:29 +0800

    create buy/sell form

--- src/pages/trade.page.tsx
+++ src/pages/trade.page.tsx
@@ -1,4 +1,18 @@
-import { Box, Container, Grid, Popover, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  Container,
+  FormControl,
+  Grid,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Popover,
+  Slider,
+  ToggleButton,
+  ToggleButtonGroup,
+  Typography,
+} from '@mui/material';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -303,17 +317,107 @@ const Orders = (): JSX.Element => {
 };
 
 const BuySellForm = (): JSX.Element => {
+  const [orderType, setOrderType] = useState('buy');
+
+  const marks = [
+    {
+      value: 0,
+      label: '0%',
+    },
+    {
+      value: 25,
+      label: '25%',
+    },
+    {
+      value: 50,
+      label: '50%',
+    },
+    {
+      value: 75,
+      label: '75%',
+    },
+    {
+      value: 100,
+      label: '100%',
+    },
+  ];
+
+  const handleChange = (event: React.MouseEvent<HTMLElement>, newOrderType: string): void => {
+    if (newOrderType !== null) {
+      setOrderType(newOrderType);
+    }
+  };
+
+  function sliderText(value: number): string {
+    return `${value}%`;
+  }
+
   return (
-    <>
-      <Box
-        sx={{
-          width: '100%',
-          height: '100%',
-        }}
-      >
-        Buy/Sell
-      </Box>
-    </>
+    <Box sx={{ width: '100%', height: '100%', padding: '1rem' }}>
+      <Grid container direction="column">
+        <Grid item>
+          <ToggleButtonGroup
+            fullWidth={true}
+            size="small"
+            value={orderType}
+            exclusive
+            onChange={handleChange}
+            aria-label="Platform"
+          >
+            <ToggleButton color="success" value="buy">
+              BUY
+            </ToggleButton>
+            <ToggleButton color="error" value="sell">
+              SELL
+            </ToggleButton>
+          </ToggleButtonGroup>
+        </Grid>
+        <Grid item direction="row" sx={{ marginTop: '1rem' }}>
+          <Typography variant="subtitle1" sx={{ display: 'inline', color: '#848e9c', marginRight: '0.5rem' }}>
+            Available
+          </Typography>
+          <Typography variant="subtitle1" sx={{ display: 'inline' }}>
+            - USDT
+          </Typography>
+        </Grid>
+        <Grid>
+          <FormControl fullWidth sx={{ marginTop: '1rem' }}>
+            <OutlinedInput
+              id="price"
+              value="Market"
+              startAdornment={<InputAdornment position="start">Price</InputAdornment>}
+              endAdornment={<InputAdornment position="end">USDT</InputAdornment>}
+              disabled
+              sx={{ backgroundColor: 'black' }}
+            />
+          </FormControl>
+          <FormControl fullWidth sx={{ marginTop: '1rem' }}>
+            <OutlinedInput
+              id="amount"
+              color="primary"
+              startAdornment={<InputAdornment position="start">Amount</InputAdornment>}
+              endAdornment={<InputAdornment position="end">BTC</InputAdornment>}
+            />
+          </FormControl>
+        </Grid>
+        <Grid item sx={{ marginTop: '1rem' }}>
+          <Slider
+            defaultValue={0}
+            getAriaValueText={sliderText}
+            valueLabelDisplay="auto"
+            step={25}
+            min={0}
+            max={100}
+            marks
+          ></Slider>
+        </Grid>
+        <Grid item sx={{ marginTop: '1rem' }}>
+          <Button fullWidth variant="contained" color="success">
+            BUY
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
